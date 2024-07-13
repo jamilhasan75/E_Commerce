9f67407a171b0251d2e16befc709def0fb961c85
jamilhasan75 <jamilhossain9393@gmail.com>
2024-07-13 22:47:30 +0600

    create database

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from main_app.models import *
 
 # Register your models here.
+admin.site.register(ContactInfo)
--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from main_app.views import index, contact
 
 urlpatterns = [
     path('',index, name='home_page'),
-    path('contact',contact, name='contact_page'),
+    path('contact/',contact, name='contact_page'),
 ]
