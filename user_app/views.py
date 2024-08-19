93fa8eca44b9e3d52b166045f5c72539898a4b9a
jamilhasan75 <jamilhossain9393@gmail.com>
2024-08-19 20:15:45 +0600

    login page updated

--- user_app/views.py
+++ user_app/views.py
@@ -25,12 +25,15 @@ def login(request):
             auth_login(request, user)
             return redirect('contact_page')
         else:
-            return HttpResponse("Username or Password is incorrect!!!")
+            context = {
+                'error_message': 'Username or Password is incorrect!!!',
+            }
+            return render(request, 'user_app/login.html', context)
     return render(request, 'user_app/login.html')
 
 #logout 
 def logout_view(request):
     logout(request)
-    return redirect('contact_page')
+    return redirect('home_page')
 
 
