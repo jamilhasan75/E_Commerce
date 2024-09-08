5bf8468017ca42c72cfbf5e8f357a08c6a6b2afa
jamilhasan75 <jamilhossain9393@gmail.com>
2024-09-08 22:24:59 +0600

    profile page template update

--- user_app/urls.py
+++ user_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout/',logout_view, name='logout_view'),
     path('profile/',profile,name='profile-page'),
     path('profile/update/',profileupdate,name='profileupdate-page'),
+    
 ]
